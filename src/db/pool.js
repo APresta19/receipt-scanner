618e429e9c7d98f22866ca82f8506e5e8d8b652e
Andrew Presta <103297057+APresta19@users.noreply.github.com>
2025-02-27 17:59:18 -0500

    Update pool.js

--- src/db/pool.js
+++ src/db/pool.js
@@ -4,10 +4,6 @@ const { Pool } = pkg;
 // All of the following properties should be read from environment variables
 // Hardcoding them here for testing
 const thepool = new Pool({
-    host: "127.0.0.1",
-    user: "andrew",
-    database: "receipt",
-    password: "february19",
-    port: 5432
+    //pretend values are here (they are on mine)
 });
 export default thepool;
